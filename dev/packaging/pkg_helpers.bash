f986469dd1e5a9a1455ddd581539c08d8e57dfed
Ali Hassani <68103095+alihassanijr@users.noreply.github.com>
2024-02-20 11:14:06 -0500

    Build wheels for python 3.12 (#107)
    Addresses #104 .
    
    Previous PR was reverted due to typesetting error.

--- dev/packaging/pkg_helpers.bash
+++ dev/packaging/pkg_helpers.bash
@@ -54,6 +54,7 @@ setup_wheel_python() {
     3.9) python_abi=cp39-cp39 ;;
     3.10) python_abi=cp310-cp310 ;;
     3.11) python_abi=cp311-cp311 ;;
+    3.12) python_abi=cp312-cp312 ;;
     *)
       echo "Unrecognized PYTHON_VERSION=$PYTHON_VERSION"
       exit 1
