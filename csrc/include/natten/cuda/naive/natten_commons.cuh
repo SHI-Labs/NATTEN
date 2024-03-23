11f3b8ea2469b28ecef42b746560d41d9f783b1a
Ali Hassani <ahassanijr@gmail.com>
2024-03-23 19:41:30 -0400

    Double atomic add does not exist for SM50 and older
    Copying over pytorch's CAS-based atomic add for devices older than SM60.

--- csrc/include/natten/cuda/naive/natten_commons.cuh
+++ csrc/include/natten/cuda/naive/natten_commons.cuh
@@ -423,6 +423,38 @@ struct AttnMask<natten::bfloat16> {
 
 #endif
 
+//////////////////////////////////////////////////
+/// Atomics for older architectures
+//////////////////////////////////////////////////
+
+#if defined(__CUDA_ARCH__)
+
+static inline __device__ float floatOrDoubleAtomicAdd(float *address, float val) {
+  return atomicAdd(address, val);
+}
+
+static inline __device__ double floatOrDoubleAtomicAdd(double* address, double val) {
+#if (__CUDA_ARCH__ >= 600)
+  return atomicAdd(address, val);
+#else
+  // Taken from pytorch;
+  // ATen/cuda/Atomic.cuh
+  unsigned long long int* address_as_ull = (unsigned long long int*)address;
+  unsigned long long int old = *address_as_ull;
+  unsigned long long int assumed;
+
+  do {
+    assumed = old;
+    old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
+    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+  } while (assumed != old);
+
+  return __longlong_as_double(old);
+#endif
+}
+
+#endif
+
 } // namespace naive
 } // namespace cuda
 } // namespace natten
