732b791f2e6f0571f0959ec893c5c59c24be2715
Ali Hassani <ahassanijr@gmail.com>
2025-10-23 20:46:23 -0400

    Fix arm builds

--- scripts/packaging/build_all_wheels_parallel.sh
+++ scripts/packaging/build_all_wheels_parallel.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -e
-# Based on detectron2's builder:
-# github.com/facebookresearch/detectron2
 
 # Always ensure cutlass/other submods are cloned
 # before attempting to build.
@@ -18,53 +16,57 @@ build_one() {
   cu=${cu/./}
   ctk_ver=${cu/cu/}
 
-  if [[ $ctk_ver -ge 126 ]]; then
-    # (ahassani) what in god's name is manylinux2_28?
-    image_name=pytorch/manylinux2_28-builder:${target}-main
-  else
-    image_name=pytorch/manylinux-builder:${target}-main
-  fi
+  SUPPORTED_ARCHES=(
+    "linux/amd64:pytorch/manylinux2_28-builder"
+    "linux/arm64:pytorch/manylinuxaarch64-builder"
+  )
 
-  echo "Launching container with $image_name to build for torch $pytorch_ver + $cu..."
-  container_name="natten_build_"_"$cu"_"$pytorch_ver"
-
-  # Torch started supporting python 3.13 since ~2.5
-  # We are building wheels for 3.13 starting 0.21.1
-  py_versions=(3.10 3.11 3.12 3.13 3.13t)
-
-  # Torch also started shipping arm builds since 2.8.
-  SUPPORTED_ARCHES=("linux/amd64")
-
-  # TODO: arm builds are best done on an arm machine -- emulators are SLOW
-  #SUPPORTED_ARCHES=("linux/arm64")
-
-  # NOTE: I can't surpress the warning from sub
-  # when --output-delimiter is "", and I'm not
-  # spending more time on this.
-  torch_major=$(echo $pytorch_ver | cut -d "." -f 1,2  --output-delimiter="")
-
-  if [[ $torch_major -lt 27 ]]; then
-    echo "Only torch 2.7 and later are supported from now on."
+  if [[ $ctk_ver -lt 126 ]]; then
+    echo "Support for CTK < 12.6 builds has been dropped."
     exit 1
   fi
 
-  # Torch 2.9 no longer ships for python 3.9.
-  if [[ $torch_major -lt 29 ]]; then
-    py_versions+=(3.9)
-  fi
+  for ARCH_CONTAINER in "${SUPPORTED_ARCHES[@]}";do
+    arr=(${ARCH_CONTAINER//:/ });
 
-  for py in "${py_versions[@]}"; do
-    for arch_tag in "${SUPPORTED_ARCHES[@]}";do
-      arch_tag_f=${arch_tag/\//-}
-      container_name_="${container_name}_${py}_${arch_tag_f}"
+    arch_tag="${arr[0]}"
+    arch_tag_f=${arch_tag/\//-}
 
+    image_name="${arr[1]}:${target}-main"
+
+    echo "Launching container with $image_name to build for torch $pytorch_ver + $cu..."
+    container_name="natten_build_"_"$cu"_"$pytorch_ver_${arch_tag_f}"
+
+    # Torch started supporting python 3.13 since ~2.5
+    # We are building wheels for 3.13 starting 0.21.1
+    py_versions=(3.10 3.11 3.12 3.13 3.13t)
+
+    # NOTE: I can't suppress the warning from sub
+    # when --output-delimiter is "", and I'm not
+    # spending more time on this.
+    torch_major=$(echo $pytorch_ver | cut -d "." -f 1,2  --output-delimiter="")
+
+    if [[ $torch_major -lt 27 ]]; then
+      echo "Only torch 2.7 and later are supported from now on."
+      exit 1
+    fi
+
+    # Torch 2.9 no longer ships python 3.9 wheels.
+    if [[ $torch_major -lt 29 ]]; then
+      py_versions+=(3.9)
+    fi
+
+    for py in "${py_versions[@]}"; do
+      container_name="${container_name}_${py}"
+
+        #--gpus=all \
       docker run -itd --rm \
         --platform $arch_tag \
-        --gpus=all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
-        --name "$container_name_" \
+        --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
+        --name "$container_name" \
         --mount type=bind,source="$(pwd)",target=/natten \
         $image_name
-      cat <<EOF | docker exec -i $container_name_ sh
+      cat <<EOF | docker exec -i $container_name sh
         export CU_VERSION=$cu PYTHON_VERSION=$py
         export PYTORCH_VERSION=$pytorch_ver
         export OUTPUT_DIR=/natten
@@ -73,7 +75,7 @@ build_one() {
         make clean &&
         ./scripts/packaging/build_wheel.sh
 EOF
-      docker container stop $container_name_
+      docker container stop $container_name
     done
   done
 }
