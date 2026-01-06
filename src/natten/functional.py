Fix merge attention backprop (#299)
Replaces logsumexp and outputs from the original operator with merged
ones in the new autograd function for attention merge (pair inputs only
for now because autograd), which is also compatible with torch compile.

Adds new tests.

Exposes 'return_lse' in na ops.

Closes #274

src/natten/functional.py
@@ -20,12 +20,12 @@
 # SOFTWARE.
 #
 #################################################################################################
-from typing import Dict, List, Optional, Tuple, Union
+from typing import Dict, Optional, Tuple, Union
 
 import torch
 from torch import Tensor
 
-from .attn_merge import merge_attentions_compile, merge_attentions_fn
+from .attn_merge import merge_attentions
 from .backends import (
     choose_backend,
     choose_fmha_backend,
@@ -348,79 +348,6 @@ def attention(
     raise NotImplementedError(f"Unrecognized NATTEN FMHA backend {backend}.")
 
 
-def merge_attentions(
-    outputs: List[Tensor], lse_tensors: List[Tensor], torch_compile: bool = True
-) -> Tensor:
-    """Takes multiple attention *outputs* originating from the same query tensor, and their
-    corresponding logsumexps, and merges them as if their context (key/value pair) had been
-    concatenated.
-
-    This operation is used to implement cross-neighborhood attention, and can also be used for
-    distributed setups, such as context-parallelism.
-
-    This operation also attempts to use `torch.compile` to fuse the elementwise operations. This
-    can be disabled by passing `torch_compile=False`.
-
-    Parameters:
-        outputs (List[Tensor]): List of 4-D attention output tensors, with the heads last layout
-            (`[batch, seqlen, heads, head_dim]`)
-
-        lse_tensors (List[Tensor]): List of 3-D logsumexp tensors, with the heads last layout
-            (`[batch, seqlen, heads]`)
-
-        torch_compile (bool): Attempt to use `torch.compile` to fuse the underlying elementwise
-            operations.
-
-    Returns:
-        output (Tensor): merged attention output.
-    """
-
-    if len(outputs) < 2:
-        raise ValueError("`merge_attentions` expects at least two tensors.")
-
-    if len(outputs) != len(lse_tensors):
-        raise ValueError(
-            "`merge_attentions` expected number of outputs and LSE tensors to match, "
-            f"got {len(outputs)=} != {len(lse_tensors)}."
-        )
-
-    for i, (output, lse) in enumerate(zip(outputs, lse_tensors)):
-        if output.dim() != 4 or not output.is_contiguous():
-            raise ValueError(
-                "Output tensors must be rank-4 tensors with (batch, seq, heads, dim), "
-                f"but got output {i} with rank={output.dim()}."
-            )
-
-        if output.shape != outputs[0].shape:
-            raise ValueError(
-                f"Output tensors must must match in shape, but got output {i} "
-                f"with shape={output.shape}."
-            )
-
-        if lse.dim() != 3:
-            raise ValueError(
-                "LSE tensors must be rank-3 tensors with (batch, seq, heads)"
-                f"but got LSE {i} with rank={lse.dim()}."
-            )
-
-        if lse.shape != outputs[0].shape[:3]:
-            raise ValueError(
-                f"LSE tensors must must match outputs in shape except last dim "
-                f"({outputs[0].shape=}), but got LSE {i} with shape={lse.shape}."
-            )
-
-    if not torch_compile:
-        return merge_attentions_fn(
-            [output.contiguous() for output in outputs],
-            [lse.contiguous() for lse in lse_tensors],
-        )
-
-    return merge_attentions_compile(
-        [output.contiguous() for output in outputs],
-        [lse.contiguous() for lse in lse_tensors],
-    )
-
-
 # Neighborhood Attention
 
 
@@ -447,7 +374,8 @@ def neighborhood_attention_generic(
     run_persistent_kernel: bool = True,
     kernel_schedule: Optional[Union[str, KernelSchedule]] = None,
     torch_compile: bool = False,
-) -> Tensor:
+    return_lse: bool = False,
+) -> Union[Tensor, Tuple[Tensor, Tensor]]:
 
     na_tensor_checks(query, key, value)
     additional_kv_tensor_checks(query, key, value, additional_keys, additional_values)
@@ -496,24 +424,31 @@ def neighborhood_attention_generic(
             value = torch.cat([value, additional_values], dim=1)
 
         attn_kwargs = attention_kwargs or {}
-        out: Tensor = attention(  # type: ignore[assignment]
+        out, lse = attention(
             query,
             key,
             value,
             is_causal=is_causal[0],  # NOTE: special case
             scale=scale,
-            return_lse=False,
+            return_lse=True,
             **attn_kwargs,
         )
-        output_shape = [s for s in query_shape[:-1]] + [value.shape[-1]]
-        return out.reshape(*output_shape)
+        lse_shape = [s for s in query_shape[:-1]]
+        output_shape = lse_shape + [value.shape[-1]]
+        out = out.reshape(*output_shape)
+        lse = lse.reshape(*lse_shape)
+
+        if return_lse:
+            return out, lse
+
+        return out
 
     scale = scale or query.shape[-1] ** -0.5
 
     backend = backend or choose_backend(query, key, value, torch_compile=torch_compile)
 
     if backend == "blackwell-fna":
-        outputs = cutlass_blackwell_fna_generic(
+        output, lse = cutlass_blackwell_fna_generic(
             query=query,
             key=key,
             value=value,
@@ -527,11 +462,11 @@ def neighborhood_attention_generic(
             backward_q_tile_shape=backward_q_tile_shape,
             backward_kv_tile_shape=backward_kv_tile_shape,
             run_persistent_kernel=run_persistent_kernel,
-            return_lse=has_additional_attention,
+            return_lse=True,
         )
 
     elif backend == "hopper-fna":
-        outputs = cutlass_hopper_fna_generic(
+        output, lse = cutlass_hopper_fna_generic(
             query=query,
             key=key,
             value=value,
@@ -545,11 +480,11 @@ def neighborhood_attention_generic(
             backward_q_tile_shape=backward_q_tile_shape,
             backward_kv_tile_shape=backward_kv_tile_shape,
             kernel_schedule=kernel_schedule,
-            return_lse=has_additional_attention,
+            return_lse=True,
         )
 
     elif backend == "cutlass-fna":
-        outputs = cutlass_fna_generic(
+        output, lse = cutlass_fna_generic(
             query=query,
             key=key,
             value=value,
@@ -564,11 +499,11 @@ def neighborhood_attention_generic(
             backward_kv_tile_shape=backward_kv_tile_shape,
             backward_kv_splits=backward_kv_splits,
             backward_use_pt_reduction=backward_use_pt_reduction,
-            return_lse=has_additional_attention,
+            return_lse=True,
         )
 
     elif backend == "flex-fna":
-        outputs = flex_fna_generic(
+        output, lse = flex_fna_generic(
             query=query,
             key=key,
             value=value,
@@ -580,7 +515,7 @@ def neighborhood_attention_generic(
             q_tile_shape=q_tile_shape,
             kv_tile_shape=kv_tile_shape,
             torch_compile=torch_compile,
-            return_lse=has_additional_attention,
+            return_lse=True,
         )
 
     else:
@@ -589,10 +524,11 @@ def neighborhood_attention_generic(
     if has_additional_attention:
         assert additional_keys is not None
         assert additional_values is not None
-        assert outputs is not None and isinstance(outputs, tuple) and len(outputs) == 2
-        out, lse = outputs
 
         attention_kwargs = attention_kwargs or {}
+        if "torch_compile" not in attention_kwargs:
+            attention_kwargs["torch_compile"] = torch_compile
+
         additional_output, additional_lse = attention(
             query.flatten(1, na_dim),
             additional_keys,
@@ -603,15 +539,29 @@ def neighborhood_attention_generic(
             **attention_kwargs,
         )
 
-        merged_output = merge_attentions(
-            [out.flatten(1, na_dim), additional_output],
-            [lse.flatten(1, na_dim), additional_lse],
+        # NOTE: Flex unfused should not use the autograd fix
+        is_flex = backend == "flex-fna" or (
+            "backend" in attention_kwargs and attention_kwargs["backend"] == "flex-fmha"
         )
-        return merged_output.reshape(out.shape)
+        use_autograd_fix = not is_flex or torch_compile
 
-    assert isinstance(outputs, Tensor)
+        merged_output, merged_lse = merge_attentions(
+            [output.flatten(1, na_dim), additional_output],
+            [lse.flatten(1, na_dim), additional_lse],
+            use_autograd_fix=use_autograd_fix,
+        )
+        merged_output = merged_output.reshape(output.shape)
+        merged_lse = merged_lse.reshape(output.shape[:-1])
 
-    return outputs
+        if return_lse:
+            return merged_output, merged_lse
+
+        return merged_output
+
+    if return_lse:
+        return output, lse
+
+    return output
 
 
 def na1d(
@@ -636,7 +586,8 @@ def na1d(
     run_persistent_kernel: bool = True,
     kernel_schedule: Optional[Union[str, KernelSchedule]] = None,
     torch_compile: bool = False,
-) -> Tensor:
+    return_lse: bool = False,
+) -> Union[Tensor, Tuple[Tensor, Tensor]]:
     """Computes 1-D neighborhood attention.
 
     GQA/MQA support (`heads != heads_kv`) is available. For now, `blackwell-fna` and
@@ -766,9 +717,15 @@ def na1d(
                 )
                 ```
 
+        return_lse (bool): Whether or not to return the `logsumexp` tensor. `logsumexp` can be used
+            in the backward pass, and for [attention merging][natten.merge_attentions].
+
     Returns:
         output (Tensor): 4-D output tensor, with the heads last layout
             (`[batch, seqlen, heads, head_dim_v]`).
+
+        logsumexp (Tensor): only returned when `return_lse=True`. 3-D logsumexp tensor, with the
+            heads last layout (`[batch, seqlen, heads]`).
     """
     return neighborhood_attention_generic(
         query=query,
@@ -792,6 +749,7 @@ def na1d(
         run_persistent_kernel=run_persistent_kernel,
         kernel_schedule=kernel_schedule,
         torch_compile=torch_compile,
+        return_lse=return_lse,
     )
 
 
@@ -817,7 +775,8 @@ def na2d(
     run_persistent_kernel: bool = True,
     kernel_schedule: Optional[Union[str, KernelSchedule]] = None,
     torch_compile: bool = False,
-) -> Tensor:
+    return_lse: bool = False,
+) -> Union[Tensor, Tuple[Tensor, Tensor]]:
     """Computes 2-D neighborhood attention.
 
     GQA/MQA support (`heads != heads_kv`) is available. For now, `blackwell-fna` and
@@ -957,9 +916,15 @@ def na2d(
                 )
                 ```
 
+        return_lse (bool): Whether or not to return the `logsumexp` tensor. `logsumexp` can be used
+            in the backward pass, and for [attention merging][natten.merge_attentions].
+
     Returns:
         output (Tensor): 5-D output tensor, with the heads last layout
             (`[batch, X, Y, heads, head_dim_v]`).
+
+        logsumexp (Tensor): only returned when `return_lse=True`. 4-D logsumexp tensor, with the
+            heads last layout (`[batch, X, Y, heads]`).
     """
     return neighborhood_attention_generic(
         query=query,
@@ -983,6 +948,7 @@ def na2d(
         run_persistent_kernel=run_persistent_kernel,
         kernel_schedule=kernel_schedule,
         torch_compile=torch_compile,
+        return_lse=return_lse,
     )
 
 
@@ -1008,7 +974,8 @@ def na3d(
     run_persistent_kernel: bool = True,
     kernel_schedule: Optional[Union[str, KernelSchedule]] = None,
     torch_compile: bool = False,
-) -> Tensor:
+    return_lse: bool = False,
+) -> Union[Tensor, Tuple[Tensor, Tensor]]:
     """Computes 3-D neighborhood attention.
 
     GQA/MQA support (`heads != heads_kv`) is available. For now, `blackwell-fna` and
@@ -1148,9 +1115,15 @@ def na3d(
                 )
                 ```
 
+        return_lse (bool): Whether or not to return the `logsumexp` tensor. `logsumexp` can be used
+            in the backward pass, and for [attention merging][natten.merge_attentions].
+
     Returns:
         output (Tensor): 6-D output tensor, with the heads last layout
             (`[batch, X, Y, Z, heads, head_dim_v]`).
+
+        logsumexp (Tensor): only returned when `return_lse=True`. 5-D logsumexp tensor, with the
+            heads last layout (`[batch, X, Y, Z, heads]`).
     """
     return neighborhood_attention_generic(
         query=query,
@@ -1174,4 +1147,5 @@ def na3d(
         run_persistent_kernel=run_persistent_kernel,
         kernel_schedule=kernel_schedule,
         torch_compile=torch_compile,
+        return_lse=return_lse,
     )

src/natten/modules.py
@@ -78,13 +78,15 @@ class NeighborhoodAttentionGeneric(nn.Module):
 
         self.expected_input_tensor_rank = self.na_dim + 2  # batch, embedding dim
 
-        self.qkv = nn.Linear(self.embed_dim, self.embed_dim * 3, bias=qkv_bias)
+        self.q = nn.Linear(self.embed_dim, self.embed_dim, bias=qkv_bias)
+        self.kv = nn.Linear(self.embed_dim, self.embed_dim * 2, bias=qkv_bias)
         self.proj = nn.Linear(self.embed_dim, self.embed_dim)
         self.proj_drop = nn.Dropout(proj_drop)
 
     def forward(
         self,
         x: Tensor,
+        additional_context: Optional[Tensor] = None,
         # SDPA fast path
         attention_kwargs: Optional[Dict] = None,
         # Optional perf-related args
@@ -113,19 +115,43 @@ class NeighborhoodAttentionGeneric(nn.Module):
                 f"Expected embedding dimension {self.embed_dim}, got {C} ({x.shape=})."
             )
 
+        # batch, *input_shape, heads, head_dim
+        permutation_q = (
+            [0]
+            + [x + 1 for x in range(self.na_dim)]
+            + [self.na_dim + 1, self.na_dim + 2]
+        )
+
         # 3, batch, *input_shape, heads, head_dim
-        permutation = (
+        permutation_kv = (
             [self.na_dim + 1, 0]
             + [x + 1 for x in range(self.na_dim)]
             + [self.na_dim + 2, self.na_dim + 3]
         )
-        qkv = (
-            self.qkv(x)
-            .reshape(B, *input_shape, 3, self.num_heads, self.head_dim)
-            .permute(*permutation)
+
+        q = (
+            self.q(x)
+            .reshape(B, *input_shape, self.num_heads, self.head_dim)
+            .permute(*permutation_q)
         )
-        q, k, v = qkv[0], qkv[1], qkv[2]
-        x = neighborhood_attention_generic(
+
+        kv = (
+            self.kv(x)
+            .reshape(B, *input_shape, 2, self.num_heads, self.head_dim)
+            .permute(*permutation_kv)
+        )
+        k, v = kv[0], kv[1]
+
+        add_k, add_v = None, None
+        if additional_context is not None:
+            add_kv = (
+                self.kv(additional_context)
+                .reshape(B, -1, 2, self.num_heads, self.head_dim)
+                .permute(2, 0, 1, 3, 4)
+            )
+            add_k, add_v = add_kv[0], add_kv[1]
+
+        x = neighborhood_attention_generic(  #  type: ignore[assignment]
             q,
             k,
             v,
@@ -147,6 +173,8 @@ class NeighborhoodAttentionGeneric(nn.Module):
             run_persistent_kernel=run_persistent_kernel,
             kernel_schedule=kernel_schedule,
             torch_compile=torch_compile,
+            additional_keys=add_k,
+            additional_values=add_v,
         )
         x = x.reshape(B, *input_shape, C)
 

tests/test_fna.py
@@ -111,12 +111,42 @@ class FNABackendTest(unittest.TestCase):
             dtype=torch.float32,
         )
 
-        # TODO: write note on why backprop eps is different when additional_kv_length > 0
-        is_gqa = heads_kv is not None and heads != heads_kv
+        # GQA is implemented with graph transformations, and will therefore
+        # have a different error threshold since it involves reductions in bwd pass.
+        # MLA will also involve a reduction in bwd pass and needs higher error thresholds
+        is_gqa_or_mla = (
+            heads_kv is not None and heads != heads_kv
+        ) or head_dim_v != head_dim
+
+        # Additional KVs are handled with a separate attention op and attention merging,
+        # but we shouldn't need beyond 1e-1 threshold (when correcting autograd, which natten
+        # does in all libnatten backends.)
+        # There is a separate attention merging test that compares backends to themselves, and can
+        # therefore have a higher threshold.
+        has_add_kv = additional_kv_length > 0
+
+        # (dtype, is_gqa_or_mla, has_additional_kv) -> atol_dq, atol_dk, atol_dv
+        bwd_atols = {
+            (torch.float32, False, False): (1e-2, 1e-4, 1e-4),
+            (torch.float32, False, True): (1e-1, 1e-1, 1e-1),
+            (torch.float32, True, False): (7e-1, 7e-1, 7e-1),
+            (torch.float32, True, True): (7e-1, 7e-1, 7e-1),
+            #
+            (torch.float16, False, False): (1e-1, 1e-2, 1e-2),
+            (torch.float16, False, True): (1e-1, 1e-1, 1e-1),
+            (torch.float16, True, False): (7e-1, 7e-1, 7e-1),
+            (torch.float16, True, True): (7e-1, 7e-1, 7e-1),
+            #
+            (torch.bfloat16, False, False): (1e-1, 1e-2, 1e-2),
+            (torch.bfloat16, False, True): (1e-1, 1e-1, 1e-1),
+            (torch.bfloat16, True, False): (7e-1, 7e-1, 7e-1),
+            (torch.bfloat16, True, True): (7e-1, 7e-1, 7e-1),
+        }
+
         ALLOWED_DTYPES = [
             (
                 torch.float32,
-                (1e-4, 1e-4 if not is_gqa and additional_kv_length == 0 else 7e-1),
+                (1e-4, bwd_atols[(torch.float32, is_gqa_or_mla, has_add_kv)]),
             ),
         ]
 
@@ -124,7 +154,7 @@ class FNABackendTest(unittest.TestCase):
             ALLOWED_DTYPES.append(
                 (
                     torch.float16,
-                    (1e-2, 1e-2 if not is_gqa and additional_kv_length == 0 else 7e-1),
+                    (1e-2, bwd_atols[(torch.float16, is_gqa_or_mla, has_add_kv)]),
                 )
             )
 
@@ -132,7 +162,7 @@ class FNABackendTest(unittest.TestCase):
             ALLOWED_DTYPES.append(
                 (
                     torch.bfloat16,
-                    (1e-1, 1e-1 if not is_gqa and additional_kv_length == 0 else 7e-1),
+                    (1e-1, bwd_atols[(torch.bfloat16, is_gqa_or_mla, has_add_kv)]),
                 )
             )
 

tests/utils.py
@@ -237,7 +237,9 @@ class NattenBackendTester:
 
     def test(
         self,
-        eps: Union[float, Tuple[float, float]],
+        eps: Union[
+            float, Tuple[float, float], Tuple[float, Tuple[float, float, float]]
+        ],
         dtype: torch.dtype,
         target_backend: str,
         target_fmha_backend: str,
@@ -306,28 +308,30 @@ class NattenBackendTester:
         torch.cuda.synchronize()
         start_time = time.time()
 
-        out_ = neighborhood_attention_generic(
-            q,
-            k,
-            v,
-            kernel_size=kernel_size,
-            stride=stride,
-            dilation=dilation,
-            is_causal=is_causal,
-            additional_keys=additional_k,
-            additional_values=additional_v,
-            #
-            backend=target_backend,
-            q_tile_shape=q_tile_shape,
-            kv_tile_shape=kv_tile_shape,
-            backward_q_tile_shape=backward_q_tile_shape,
-            backward_kv_tile_shape=backward_kv_tile_shape,
-            backward_kv_splits=backward_kv_splits,
-            backward_use_pt_reduction=backward_use_pt_reduction,
-            run_persistent_kernel=run_persistent_kernel,
-            kernel_schedule=kernel_schedule,
-            torch_compile=torch_compile,
-            attention_kwargs={"backend": target_fmha_backend},
+        out_: torch.Tensor = (
+            neighborhood_attention_generic(  #  type: ignore[assignment]
+                q,
+                k,
+                v,
+                kernel_size=kernel_size,
+                stride=stride,
+                dilation=dilation,
+                is_causal=is_causal,
+                additional_keys=additional_k,
+                additional_values=additional_v,
+                #
+                backend=target_backend,
+                q_tile_shape=q_tile_shape,
+                kv_tile_shape=kv_tile_shape,
+                backward_q_tile_shape=backward_q_tile_shape,
+                backward_kv_tile_shape=backward_kv_tile_shape,
+                backward_kv_splits=backward_kv_splits,
+                backward_use_pt_reduction=backward_use_pt_reduction,
+                run_persistent_kernel=run_persistent_kernel,
+                kernel_schedule=kernel_schedule,
+                torch_compile=torch_compile,
+                attention_kwargs={"backend": target_fmha_backend},
+            )
         )
         out = out_.data.clone().float()
 
@@ -366,15 +370,22 @@ class NattenBackendTester:
         torch.testing.assert_close(out, self.out_ref, atol=eps_forward, rtol=0)
 
         if test_backprop_safe:
-            torch.testing.assert_close(dq, self.dq_ref, atol=eps_backward, rtol=0)
-            torch.testing.assert_close(dk, self.dk_ref, atol=eps_backward, rtol=0)
-            torch.testing.assert_close(dv, self.dv_ref, atol=eps_backward, rtol=0)
+            if isinstance(eps_backward, tuple):
+                assert len(eps_backward) == 3
+                eps_dq, eps_dk, eps_dv = eps_backward
+            else:
+                assert isinstance(eps_backward, float)
+                eps_dq, eps_dk, eps_dv = eps_backward, eps_backward, eps_backward
+
+            torch.testing.assert_close(dq, self.dq_ref, atol=eps_dq, rtol=0)
+            torch.testing.assert_close(dk, self.dk_ref, atol=eps_dk, rtol=0)
+            torch.testing.assert_close(dv, self.dv_ref, atol=eps_dv, rtol=0)
             if additional_kv_length > 0:
                 torch.testing.assert_close(
-                    d_additional_k, self.d_additional_k_ref, atol=eps_backward, rtol=0
+                    d_additional_k, self.d_additional_k_ref, atol=eps_dk, rtol=0
                 )
                 torch.testing.assert_close(
-                    d_additional_v, self.d_additional_v_ref, atol=eps_backward, rtol=0
+                    d_additional_v, self.d_additional_v_ref, atol=eps_dv, rtol=0
                 )
 
 