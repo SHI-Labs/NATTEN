b05994f8752708d1721fe31f9884ba1bb5ab277c
Ali Hassani <ahassanijr@gmail.com>
2026-03-17 14:13:01 -0700

    some other stuff

--- scripts/misc/update-copyright-year.sh
+++ scripts/misc/update-copyright-year.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set -e
 
 find src/ tests/ setup.py -type f \
   -name '*.py' \
-  -exec sed -i 's/Copyright (c) 2022 - 2026 Ali Hassani./Copyright (c) 2022 - 2026 Ali Hassani./g' {} \;
+  -exec sed -i 's/\(Copyright (c) 2022\) - 2025 \(Ali Hassani.\)/\1 - 2026 \2/g' {} \;
 
 find csrc/ -type f \
   \( -name '*.cu' -o \
@@ -13,13 +14,24 @@ find csrc/ -type f \
   -name '*.hpp' -o \
   -name '*.txt' -o \
   -name '*.cc' \) \
-  -exec sed -i 's/Copyright (c) 2022 - 2026 Ali Hassani./Copyright (c) 2022 - 2026 Ali Hassani./g' {} \;
+  -exec sed -i 's/\(Copyright (c) 2022 -\) 2025 \(Ali Hassani.\)/\1 2026 \2/g' {} \;
 
 find scripts/ -type f \
-  -exec sed -i 's/Copyright (c) 2022 - 2026 Ali Hassani./Copyright (c) 2022 - 2026 Ali Hassani./g' {} \;
+  -exec sed -i 's/\(Copyright (c) 2022 -\) 2025 \(Ali Hassani.\)/\1 2026 \2/g' {} \;
 
 find LICENSE NOTICE Dockerfile Makefile -type f \
-  -exec sed -i 's/Copyright (c) 2022 - 2025 Ali Hassani./Copyright (c) 2022 - 2026 Ali Hassani./g' {} \;
+  -exec sed -i 's/\(Copyright (c) 2022 -\) 2025 \(Ali Hassani.\)/\1 2026 \2/g' {} \;
 
 find mkdocs.yml -type f \
-  -exec sed -i 's/2022 - 2025 Ali Hassani/2022 - 2026 Ali Hassani/g' {} \;
+  -exec sed -i 's/\(2022 -\) 2025 \(Ali Hassani\)/\1 2026 \2/g' {} \;
+
+# nvidia copyrights
+find csrc/ -type f \
+  \( -name '*.cu' -o \
+  -name '*.cpp' -o \
+  -name '*.h' -o \
+  -name '*.cuh' -o \
+  -name '*.hpp' -o \
+  -name '*.txt' -o \
+  -name '*.cc' \) \
+  -exec sed -i 's/\(Copyright (c) 20[0-9][0-9] -\) 20[0-9][0-9] \(NVIDIA CORPORATION\)/\1 2026 \2/g' {} \;
