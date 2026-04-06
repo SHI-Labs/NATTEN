b76d5e4c8cbc098d0238f6a73646979bd83256c4
Ali Hassani <ahassanijr@gmail.com>
2026-04-06 15:30:44 -0700

    Fix ref kernel bug
    Reference FNA/FMHA kernels were implementing GQA/MQA incorrectly. Grid
    parallelism was set up across all heads instead of KV heads, and in
    addition to the race condition, gradients weren't accumulated because
    the dK/dV kernels were overwriting conflicting regions.
    
    Necessary corrections were made, and we can now get rid of the special
    rule for GQA/MQA tolerances that we incorrectly attributed to the
    reduction. I guess I too am guilty of the crime of playing with
    tolerances just to make my tests pass, but hopefully no more.
    
    Also redid the randsweep use case generator so it generates more
    sensible input shapes, and adjusts heads/batch accordingly so we don't
    have to wait 12 hours for the extended suite to pass.

--- csrc/include/natten/cuda/reference/fna_reference_backward.hpp
+++ csrc/include/natten/cuda/reference/fna_reference_backward.hpp
@@ -89,70 +89,73 @@ void __global__ fna_bwd_reference_dQ_kernel(
 
   cutlass::Array<ElementAccumulator, DimPerThread> final_acc;
 
-  for (int idx_L = blockIdx.y; idx_L < size<2>(mDQ); idx_L += gridDim.y) {
-    for (int idx_Q = blockIdx.x; idx_Q < size<0>(mDQ); idx_Q += gridDim.x) {
-      final_acc.clear();
-      auto num_kv_tiles = ceil_div(size<0>(mK), KVTileSize);
-      for (int kv_tile = 0; kv_tile < num_kv_tiles; ++kv_tile) {
-        auto kv_tile_offset = kv_tile * KVTileSize;
-        auto kv_tile_max = kv_tile_offset + KVTileSize;
-        auto K_max = std::min(kv_tile_max, size<0>(mK));
-        auto mS_max = K_max - kv_tile_offset;
+  for (int idx_H = blockIdx.z; idx_H < size<2, 1>(mDQ); idx_H += gridDim.z) {
+    for (int idx_B = blockIdx.y; idx_B < size<2, 0>(mDQ); idx_B += gridDim.y) {
+      auto idx_L = make_tuple(idx_B, idx_H);
+      for (int idx_Q = blockIdx.x; idx_Q < size<0>(mDQ); idx_Q += gridDim.x) {
+        final_acc.clear();
+        auto num_kv_tiles = ceil_div(size<0>(mK), KVTileSize);
+        for (int kv_tile = 0; kv_tile < num_kv_tiles; ++kv_tile) {
+          auto kv_tile_offset = kv_tile * KVTileSize;
+          auto kv_tile_max = kv_tile_offset + KVTileSize;
+          auto K_max = std::min(kv_tile_max, size<0>(mK));
+          auto mS_max = K_max - kv_tile_offset;
 
-        for (int idx_K = kv_tile_offset + threadIdx.x; idx_K < K_max;
-             idx_K += blockDim.x) {
-          ElementAccumulator acc_qk = 0;
-          ElementAccumulator acc_dov = 0;
-          ElementAccumulator acc_doo = 0;
-          for (int idx_D0 = 0; idx_D0 < size<1>(mK); idx_D0++) {
-            acc_qk += mQ(idx_Q, idx_D0, idx_L) * mK(idx_K, idx_D0, idx_L);
-          } // for idx_D0
-          for (int idx_D1 = 0; idx_D1 < size<1>(mV); idx_D1++) {
-            acc_dov += mDO(idx_Q, idx_D1, idx_L) * mV(idx_K, idx_D1, idx_L);
-            acc_doo += mDO(idx_Q, idx_D1, idx_L) * mO(idx_Q, idx_D1, idx_L);
-          } // for idx_D1
-          acc_qk *= attn_scale;
-          acc_dov *= attn_scale;
-          acc_doo *= attn_scale;
+          for (int idx_K = kv_tile_offset + threadIdx.x; idx_K < K_max;
+               idx_K += blockDim.x) {
+            ElementAccumulator acc_qk = 0;
+            ElementAccumulator acc_dov = 0;
+            ElementAccumulator acc_doo = 0;
+            for (int idx_D0 = 0; idx_D0 < size<1>(mK); idx_D0++) {
+              acc_qk += mQ(idx_Q, idx_D0, idx_L) * mK(idx_K, idx_D0, idx_L);
+            } // for idx_D0
+            for (int idx_D1 = 0; idx_D1 < size<1>(mV); idx_D1++) {
+              acc_dov += mDO(idx_Q, idx_D1, idx_L) * mV(idx_K, idx_D1, idx_L);
+              acc_doo += mDO(idx_Q, idx_D1, idx_L) * mO(idx_Q, idx_D1, idx_L);
+            } // for idx_D1
+            acc_qk *= attn_scale;
+            acc_dov *= attn_scale;
+            acc_doo *= attn_scale;
 
-          auto id = make_identity_tensor(make_shape(1, 1));
-          auto frag = make_tensor<ElementAccumulator>(Shape<_1, _1>{});
-          frag(0) = acc_qk;
-          attention_mask.apply_mask(
-              frag,
-              make_tensor(
-                  id.data() + make_arithmetic_tuple(idx_Q, idx_K),
-                  id.layout()));
-          acc_qk = frag(0);
+            auto id = make_identity_tensor(make_shape(1, 1));
+            auto frag = make_tensor<ElementAccumulator>(Shape<_1, _1>{});
+            frag(0) = acc_qk;
+            attention_mask.apply_mask(
+                frag,
+                make_tensor(
+                    id.data() + make_arithmetic_tuple(idx_Q, idx_K),
+                    id.layout()));
+            acc_qk = frag(0);
 
-          mS[idx_K - kv_tile_offset] = static_cast<Element>(
-              exp(acc_qk - mLSE(idx_Q, idx_L)) * (acc_dov - acc_doo));
-        } // for idx_K
+            mS[idx_K - kv_tile_offset] = static_cast<Element>(
+                exp(acc_qk - mLSE(idx_Q, idx_L)) * (acc_dov - acc_doo));
+          } // for idx_K
 
-        __syncthreads();
+          __syncthreads();
+
+          for (int i = 0; i < DimPerThread; ++i) {
+            int idx_D = threadIdx.x + i * blockDim.x;
+            if (idx_D < size<1>(mDQ)) {
+              for (int j = 0; j < mS_max; j++) {
+                int idx_K = j + kv_tile_offset;
+                final_acc[i] += mS[j] * mK(idx_K, idx_D, idx_L);
+              }
+            }
+          } // for idx_D
+
+          __syncthreads();
+        }
 
         for (int i = 0; i < DimPerThread; ++i) {
           int idx_D = threadIdx.x + i * blockDim.x;
           if (idx_D < size<1>(mDQ)) {
-            for (int j = 0; j < mS_max; j++) {
-              int idx_K = j + kv_tile_offset;
-              final_acc[i] += mS[j] * mK(idx_K, idx_D, idx_L);
-            }
+            mDQ(idx_Q, idx_D, idx_L) =
+                static_cast<typename TensorDQ::value_type>(final_acc[i]);
           }
-        } // for idx_D
-
-        __syncthreads();
-      }
-
-      for (int i = 0; i < DimPerThread; ++i) {
-        int idx_D = threadIdx.x + i * blockDim.x;
-        if (idx_D < size<1>(mDQ)) {
-          mDQ(idx_Q, idx_D, idx_L) =
-              static_cast<typename TensorDQ::value_type>(final_acc[i]);
         }
       }
-    }
-  }
+    } // idx_B
+  } // idx_H
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -201,71 +204,75 @@ void __global__ fna_bwd_reference_dK_kernel(
 
   cutlass::Array<ElementAccumulator, DimPerThread> final_acc;
 
-  for (int idx_L = blockIdx.y; idx_L < size<2>(mDK); idx_L += gridDim.y) {
+  for (int idx_B = blockIdx.y; idx_B < size<2, 0>(mDK); idx_B += gridDim.y) {
     for (int idx_K = blockIdx.x; idx_K < size<0>(mDK); idx_K += gridDim.x) {
       final_acc.clear();
-      auto num_q_tiles = ceil_div(size<0>(mDO), QTileSize);
-      for (int q_tile = 0; q_tile < num_q_tiles; ++q_tile) {
-        auto q_tile_offset = q_tile * QTileSize;
-        auto q_tile_max = q_tile_offset + QTileSize;
-        auto Q_max = std::min(q_tile_max, size<0>(mDO));
-        auto mS_max = Q_max - q_tile_offset;
+      for (int idx_H = 0; idx_H < size<2, 1>(mDK); ++idx_H) {
+        auto idx_L = make_tuple(idx_B, idx_H);
+        auto num_q_tiles = ceil_div(size<0>(mDO), QTileSize);
+        for (int q_tile = 0; q_tile < num_q_tiles; ++q_tile) {
+          auto q_tile_offset = q_tile * QTileSize;
+          auto q_tile_max = q_tile_offset + QTileSize;
+          auto Q_max = std::min(q_tile_max, size<0>(mDO));
+          auto mS_max = Q_max - q_tile_offset;
 
-        for (int idx_Q = q_tile_offset + threadIdx.x; idx_Q < Q_max;
-             idx_Q += blockDim.x) {
-          ElementAccumulator acc_qk = 0;
-          ElementAccumulator acc_dov = 0;
-          ElementAccumulator acc_doo = 0;
-          for (int idx_D0 = 0; idx_D0 < size<1>(mK); idx_D0++) {
-            acc_qk += mQ(idx_Q, idx_D0, idx_L) * mK(idx_K, idx_D0, idx_L);
-          } // for idx_D0
-          for (int idx_D1 = 0; idx_D1 < size<1>(mV); idx_D1++) {
-            acc_dov += mDO(idx_Q, idx_D1, idx_L) * mV(idx_K, idx_D1, idx_L);
-            acc_doo += mDO(idx_Q, idx_D1, idx_L) * mO(idx_Q, idx_D1, idx_L);
-          } // for idx_D1
-          acc_qk *= attn_scale;
-          acc_dov *= attn_scale;
-          acc_doo *= attn_scale;
+          for (int idx_Q = q_tile_offset + threadIdx.x; idx_Q < Q_max;
+               idx_Q += blockDim.x) {
+            ElementAccumulator acc_qk = 0;
+            ElementAccumulator acc_dov = 0;
+            ElementAccumulator acc_doo = 0;
+            for (int idx_D0 = 0; idx_D0 < size<1>(mK); idx_D0++) {
+              acc_qk += mQ(idx_Q, idx_D0, idx_L) * mK(idx_K, idx_D0, idx_L);
+            } // for idx_D0
+            for (int idx_D1 = 0; idx_D1 < size<1>(mV); idx_D1++) {
+              acc_dov += mDO(idx_Q, idx_D1, idx_L) * mV(idx_K, idx_D1, idx_L);
+              acc_doo += mDO(idx_Q, idx_D1, idx_L) * mO(idx_Q, idx_D1, idx_L);
+            } // for idx_D1
+            acc_qk *= attn_scale;
+            acc_dov *= attn_scale;
+            acc_doo *= attn_scale;
 
-          auto id = make_identity_tensor(make_shape(1, 1));
-          auto frag = make_tensor<ElementAccumulator>(Shape<_1, _1>{});
-          frag(0) = acc_qk;
-          attention_mask.apply_mask(
-              frag,
-              make_tensor(
-                  id.data() + make_arithmetic_tuple(idx_Q, idx_K),
-                  id.layout()));
-          acc_qk = frag(0);
+            auto id = make_identity_tensor(make_shape(1, 1));
+            auto frag = make_tensor<ElementAccumulator>(Shape<_1, _1>{});
+            frag(0) = acc_qk;
+            attention_mask.apply_mask(
+                frag,
+                make_tensor(
+                    id.data() + make_arithmetic_tuple(idx_Q, idx_K),
+                    id.layout()));
+            acc_qk = frag(0);
 
-          mS[idx_Q - q_tile_offset] = static_cast<Element>(
-              exp(acc_qk - mLSE(idx_Q, idx_L)) * (acc_dov - acc_doo));
-        } // for idx_Q
+            mS[idx_Q - q_tile_offset] = static_cast<Element>(
+                exp(acc_qk - mLSE(idx_Q, idx_L)) * (acc_dov - acc_doo));
+          } // for idx_Q
 
-        __syncthreads();
+          __syncthreads();
 
-        for (int i = 0; i < DimPerThread; ++i) {
-          int idx_D = threadIdx.x + i * blockDim.x;
-          if (idx_D < size<1>(mDK)) {
-            for (int j = 0; j < mS_max; j++) {
-              int idx_Q = j + q_tile_offset;
-              final_acc[i] += mS[j] * mQ(idx_Q, idx_D, idx_L);
+          for (int i = 0; i < DimPerThread; ++i) {
+            int idx_D = threadIdx.x + i * blockDim.x;
+            if (idx_D < size<1>(mDK)) {
+              for (int j = 0; j < mS_max; j++) {
+                int idx_Q = j + q_tile_offset;
+                final_acc[i] += mS[j] * mQ(idx_Q, idx_D, idx_L);
+              }
             }
           }
+
+          __syncthreads();
         }
 
-        __syncthreads();
-      }
+      } // for idx_H
 
       for (int i = 0; i < DimPerThread; ++i) {
         int idx_D = threadIdx.x + i * blockDim.x;
         if (idx_D < size<1>(mDK)) {
-          mDK(idx_K, idx_D, idx_L) =
+          mDK(idx_K, idx_D, make_tuple(idx_B, 0)) =
               static_cast<typename TensorDK::value_type>(final_acc[i]);
         }
       }
 
     } // for idx_K
-  } // for idx_L
+  } // for idx_B
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -314,68 +321,71 @@ void __global__ fna_bwd_reference_dV_kernel(
 
   cutlass::Array<ElementAccumulator, DimPerThread> final_acc;
 
-  for (int idx_L = blockIdx.y; idx_L < size<2>(mDV); idx_L += gridDim.y) {
+  for (int idx_B = blockIdx.y; idx_B < size<2, 0>(mDV); idx_B += gridDim.y) {
     for (int idx_K = blockIdx.x; idx_K < size<0>(mDV); idx_K += gridDim.x) {
       final_acc.clear();
-      auto num_q_tiles = ceil_div(size<0>(mDO), QTileSize);
-      for (int q_tile = 0; q_tile < num_q_tiles; ++q_tile) {
-        auto q_tile_offset = q_tile * QTileSize;
-        auto q_tile_max = q_tile_offset + QTileSize;
-        auto Q_max = std::min(q_tile_max, size<0>(mDO));
-        auto mS_max = Q_max - q_tile_offset;
+      for (int idx_H = 0; idx_H < size<2, 1>(mDV); ++idx_H) {
+        auto idx_L = make_tuple(idx_B, idx_H);
+        auto num_q_tiles = ceil_div(size<0>(mDO), QTileSize);
+        for (int q_tile = 0; q_tile < num_q_tiles; ++q_tile) {
+          auto q_tile_offset = q_tile * QTileSize;
+          auto q_tile_max = q_tile_offset + QTileSize;
+          auto Q_max = std::min(q_tile_max, size<0>(mDO));
+          auto mS_max = Q_max - q_tile_offset;
 
-        for (int idx_Q = q_tile_offset + threadIdx.x; idx_Q < Q_max;
-             idx_Q += blockDim.x) {
-          ElementAccumulator acc_qk = 0;
+          for (int idx_Q = q_tile_offset + threadIdx.x; idx_Q < Q_max;
+               idx_Q += blockDim.x) {
+            ElementAccumulator acc_qk = 0;
 
-          for (int idx_D0 = 0; idx_D0 < size<1>(mK); idx_D0++) {
-            ElementAccumulator rQ = mQ(idx_Q, idx_D0, idx_L);
-            ElementAccumulator rK = mK(idx_K, idx_D0, idx_L);
-            acc_qk += rQ * rK;
-          } // for idx_D0
-          acc_qk *= attn_scale;
+            for (int idx_D0 = 0; idx_D0 < size<1>(mK); idx_D0++) {
+              ElementAccumulator rQ = mQ(idx_Q, idx_D0, idx_L);
+              ElementAccumulator rK = mK(idx_K, idx_D0, idx_L);
+              acc_qk += rQ * rK;
+            } // for idx_D0
+            acc_qk *= attn_scale;
 
-          auto id = make_identity_tensor(make_shape(1, 1));
-          auto frag = make_tensor<ElementAccumulator>(Shape<_1, _1>{});
-          frag(0) = acc_qk;
-          attention_mask.apply_mask(
-              frag,
-              make_tensor(
-                  id.data() + make_arithmetic_tuple(idx_Q, idx_K),
-                  id.layout()));
-          acc_qk = frag(0);
+            auto id = make_identity_tensor(make_shape(1, 1));
+            auto frag = make_tensor<ElementAccumulator>(Shape<_1, _1>{});
+            frag(0) = acc_qk;
+            attention_mask.apply_mask(
+                frag,
+                make_tensor(
+                    id.data() + make_arithmetic_tuple(idx_Q, idx_K),
+                    id.layout()));
+            acc_qk = frag(0);
 
-          mS[idx_Q - q_tile_offset] =
-              static_cast<Element>(exp(acc_qk - mLSE(idx_Q, idx_L)));
-        } // for idx_Q
+            mS[idx_Q - q_tile_offset] =
+                static_cast<Element>(exp(acc_qk - mLSE(idx_Q, idx_L)));
+          } // for idx_Q
 
-        __syncthreads();
+          __syncthreads();
 
-        for (int i = 0; i < DimPerThread; ++i) {
-          int idx_D = threadIdx.x + i * blockDim.x;
-          if (idx_D < size<1>(mDV)) {
-            for (int j = 0; j < mS_max; j++) {
-              int idx_Q = j + q_tile_offset;
-              ElementAccumulator rS = mS[/*idx_Q*/ j];
-              ElementAccumulator rDO = mDO(idx_Q, idx_D, idx_L);
-              final_acc[i] += rS * rDO;
+          for (int i = 0; i < DimPerThread; ++i) {
+            int idx_D = threadIdx.x + i * blockDim.x;
+            if (idx_D < size<1>(mDV)) {
+              for (int j = 0; j < mS_max; j++) {
+                int idx_Q = j + q_tile_offset;
+                ElementAccumulator rS = mS[/*idx_Q*/ j];
+                ElementAccumulator rDO = mDO(idx_Q, idx_D, idx_L);
+                final_acc[i] += rS * rDO;
+              }
             }
           }
-        }
 
-        __syncthreads();
-      }
+          __syncthreads();
+        }
+      } // for idx_H
 
       for (int i = 0; i < DimPerThread; ++i) {
         int idx_D = threadIdx.x + i * blockDim.x;
         if (idx_D < size<1>(mDV)) {
-          mDV(idx_K, idx_D, idx_L) =
+          mDV(idx_K, idx_D, make_tuple(idx_B, 0)) =
               static_cast<typename TensorDV::value_type>(final_acc[i]);
         }
       }
 
     } // for idx_K
-  } // for idx_L
+  } // for idx_B
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -423,7 +433,7 @@ void fna_bwd_reference_dQ(
       size<1>(mDQ) <= MaxDimSupported,
       "Reference kernel only supports up to head dim 1024.");
 
-  dim3 grid(size<0>(mDQ), size<2>(mDQ), 1);
+  dim3 grid(size<0>(mDQ), size<2, 0>(mDQ), size<2, 1>(mDQ));
   dim3 block(NumThreads);
   int shared_mem = KVTileSize * sizeof(typename TensorO::value_type);
 
@@ -495,7 +505,7 @@ void fna_bwd_reference_dK(
       size<1>(mDK) <= MaxDimSupported,
       "Reference kernel only supports up to head dim 1024.");
 
-  dim3 grid(size<0>(mDK), size<2>(mDK), 1);
+  dim3 grid(size<0>(mDK), size<2, 0>(mDK), 1);
   dim3 block(NumThreads);
   int shared_mem = QTileSize * sizeof(typename TensorO::value_type);
 
@@ -567,7 +577,7 @@ void fna_bwd_reference_dV(
       size<1>(mDV) <= MaxDimSupported,
       "Reference kernel only supports up to head dim 1024.");
 
-  dim3 grid(size<0>(mDV), size<2>(mDV), 1);
+  dim3 grid(size<0>(mDV), size<2, 0>(mDV), 1);
   dim3 block(NumThreads);
   int shared_mem = QTileSize * sizeof(typename TensorO::value_type);
 
@@ -627,30 +637,55 @@ void fna_reference_backward(
       seqlen,
       seqlen + num_additional_kv,
       dim,
-      cute::make_tuple(cute::make_tuple(heads_q / heads_kv, heads_kv), batch),
+      cute::make_tuple(cute::make_tuple(heads_kv, batch), heads_q / heads_kv),
       dim_value);
 
   int SQ = size<0>(problem_shape);
   int SK = size<1>(problem_shape);
   int D = size<2>(problem_shape);
   int DV = size<4>(problem_shape);
-  int H = size<3, 0>(problem_shape);
-  int H_K = size<3, 0, 1>(problem_shape);
-  int H_Q = size<3, 0, 0>(problem_shape);
-  int B = size<3, 1>(problem_shape);
+  int H_Q = size<3, 1>(problem_shape);
+  int H_K = size<3, 0, 0>(problem_shape);
+  int B = size<3, 0, 1>(problem_shape);
+  int H = heads_q;
 
   // heads last profile, with torch's "contiguous layout"
   // shape: (batch, seqlen, heads, dim)
   // stride: (dim*heads*seqlen, dim*heads, dim, 1)
   auto stride_Q = make_stride(
-      H * D, _1{}, make_stride(make_stride(D, H_Q * D), H * D * SQ));
+      H * D,
+      _1{},
+      make_stride(
+          make_stride(
+              H_Q * D, static_cast<int64_t>(H * D) * static_cast<int64_t>(SQ)),
+          D));
   auto stride_O = make_stride(
-      H * DV, _1{}, make_stride(make_stride(DV, H_Q * DV), H * DV * SQ));
+      H * DV,
+      _1{},
+      make_stride(
+          make_stride(
+              H_Q * DV,
+              static_cast<int64_t>(H * DV) * static_cast<int64_t>(SQ)),
+          DV));
   auto stride_K = make_stride(
-      H_K * D, _1{}, make_stride(make_stride(_0{}, D), H_K * D * SK));
+      H_K * D,
+      _1{},
+      make_stride(
+          make_stride(
+              D, static_cast<int64_t>(H_K * D) * static_cast<int64_t>(SK)),
+          _0{}));
   auto stride_V = make_stride(
-      H_K * DV, _1{}, make_stride(make_stride(_0{}, DV), H_K * DV * SK));
-  auto stride_LSE = make_stride(H, make_stride(make_stride(_1{}, H_Q), SQ * H));
+      H_K * DV,
+      _1{},
+      make_stride(
+          make_stride(
+              DV, static_cast<int64_t>(H_K * DV) * static_cast<int64_t>(SK)),
+          _0{}));
+  auto stride_LSE = make_stride(
+      H,
+      make_stride(
+          make_stride(H_Q, static_cast<int64_t>(SQ) * static_cast<int64_t>(H)),
+          _1{}));
 
   auto mQ = make_tensor(
       make_gmem_ptr(ptr_Q), select<0, 2, 3>(problem_shape), stride_Q);
